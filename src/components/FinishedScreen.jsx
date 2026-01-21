611f85272310ddc8dbce680b68881d8ac4d96390
Dmitriy <mr.dima337@gmail.com>
2026-01-21 14:38:28 +0300

    feat: inplement restarting quiz

--- src/components/FinishedScreen.jsx
+++ src/components/FinishedScreen.jsx
@@ -1,4 +1,4 @@
-function FinishedScreen({ points, maxPossiblePoints, highscore }) {
+function FinishedScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
   if (percentage === 100) emoji = "🎖️";
@@ -13,6 +13,7 @@ function FinishedScreen({ points, maxPossiblePoints, highscore }) {
         {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore: {highscore} points)</p>
+      <button className="btn btn-ui" onClick={()=> dispatch({type: 'restart'})}>Restart quiz</button>
     </>
   );
 }
