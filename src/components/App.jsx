657260d4eb73d582bb87095f86ed3c4954afd8cc
Dmitriy <mr.dima337@gmail.com>
2026-01-29 12:02:26 +0300

    feat: displaying startScreen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,74 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import MainContent from "./MainContent";
-import axios from 'axios'
+import axios from "axios";
+import StartScreen from "./StartScreen";
+import Loader from "./Loader";
+import Error from "./Error";
 
 const initialState = {
   questions: [],
-  status: 'loading'
-}
+  status: "loading",
+};
 
 function reducer(state, action) {
-  switch(action.type) {
-    case 'dataReceived':
+  switch (action.type) {
+    case 'loading':
       return {
         ...state,
-        questions: action.payload,
-        state: 'ready'
+        status: 'loading'
       }
-    case 'dataFailed':
+    case 'error':
       return {
         ...state,
         status: 'error'
       }
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
     default:
-      return console.log('Unknown action')
+      return console.log("Unknown action");
   }
 }
 
-
-
-
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
 
-const [state, dispatch] = useReducer(reducer, initialState)
-
-  useEffect(()=>{
-    axios.get('http://localhost:9000/questions')
-      .then(res => dispatch({type: 'dataReceived', payload: res.data}))
-      .catch(err => console.error(err))
-  }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/questions")
+      .then((res) => dispatch({ type: "dataReceived", payload: res.data }))
+      .catch((err) => dispatch({type: 'error'}));
+  }, []);
 
   return (
     <div className="app">
       <Header />
       <MainContent>
+        {status === 'error' && <Error/>}
+        {status === 'loading' && <Loader/>}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <p>Hello</p>
+          </>
+        )}
       </MainContent>
     </div>
   );
